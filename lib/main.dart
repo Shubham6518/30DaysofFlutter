8804e02d9f1a072159ff84181d16dd1eb2d5a8fb
Shubham6518 <shubhamgupta6518@gmail.com>
2023-10-06 01:35:25 +0530

    Day4 Complete

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
       themeMode: ThemeMode.light,
       theme: ThemeData(primarySwatch: Colors.deepPurple),
       // theme: ThemeData.dark(),
+      initialRoute: "/home",
       routes: {
         "/": (context) => HomePage(),
       },
