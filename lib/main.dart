69888f3d556c600101eda79cf995005fc63cb112
Shubham6518 <shubhamgupta6518@gmail.com>
2023-10-09 00:56:14 +0530

    day 8 completed

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/home_page.dart';
 import 'package:flutter_catalog/pages/login_page.dart';
