5188fe25d83bc6b71fa0eb403721ff147d059dc1
Shubham6518 <shubhamgupta6518@gmail.com>
2023-10-10 01:02:58 +0530

    day 12 completed

--- lib/models/catalog.dart
+++ lib/models/catalog.dart
@@ -1,23 +1,7 @@
-class Item {
-  final String id;
-  final String name;
-  final String desc;
-  final num price;
-  final String color;
-  final String image;
-
-  Item({
-    this.id = "",
-    this.name = '',
-    this.desc = '',
-    this.price = 1,
-    this.color = '',
-    this.image = ''});
-}
-
-final products = [
+class CatalogModel{
+  static final items = [
   Item(
-      id: "Item001",
+      id: 001,
       name: "Iphone 15 Pro",
       desc: "Iphone 15th generation.",
       price: 2000,
@@ -25,3 +9,21 @@ final products = [
       image:
           "https://fdn2.gsmarena.com/vv/pics/apple/apple-iphone-15-pro-max-1.jpg")
 ];
+}
+
+class Item {
+  final int id;
+  final String name;
+  final String desc;
+  final num price;
+  final String color;
+  final String image;
+
+  Item({
+    this.id = 001,
+    this.name = '',
+    this.desc = '',
+    this.price = 1,
+    this.color = '',
+    this.image = ''});
+}
