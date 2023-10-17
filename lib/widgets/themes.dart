35cd27dc61d1d85778d2c75416e1b4e738f9a999
Shubham6518 <shubhamgupta6518@gmail.com>
2023-10-18 02:32:06 +0530

    day17 completed

--- lib/widgets/themes.dart
+++ lib/widgets/themes.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class MyTheme {
   static ThemeData lightTheme(BuildContext context) => ThemeData(
       primarySwatch: Colors.deepPurple,
-      fontFamily: GoogleFonts.lato().fontFamily,
+      fontFamily: GoogleFonts.poppins().fontFamily,
       appBarTheme: AppBarTheme(
         centerTitle: false,
         color: Colors.white,
@@ -18,4 +18,7 @@ class MyTheme {
       ));
 
   static ThemeData darkTheme(BuildContext context) => ThemeData.dark();
+
+  static Color creamColor = Color(0xfff5f5f5);
+  static Color darkBluishColor = Color(0xff483b5a);
 }
